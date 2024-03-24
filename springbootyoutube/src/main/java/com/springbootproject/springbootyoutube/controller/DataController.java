759e4a18162608c01e08acf7391d9f823d421bd0
Ajin007 <ajinroch007@gmail.com>
2024-03-24 22:30:57 +0530

    Post method created and Requestbody added

--- springbootyoutube/src/main/java/com/springbootproject/springbootyoutube/controller/DataController.java
+++ springbootyoutube/src/main/java/com/springbootproject/springbootyoutube/controller/DataController.java
@@ -1,6 +1,8 @@
 package com.springbootproject.springbootyoutube.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,12 @@ public class DataController {
     @GetMapping("/controller")
     public String returnControllerpage(){
         return "Hi this is my first controller that conects the front ends";
+    }
+
+    // this is the post methos so request body is added
+    @PostMapping("/controller")
+    public String postMethod(@RequestBody String name){
+        return "This is the post value"+"the posted value is"+name;
     }
 
 }
