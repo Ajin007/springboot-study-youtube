b54f25b1939112fa2fc0c0bc2c57856da47f98d6
Ajin007 <ajinroch007@gmail.com>
2024-03-24 23:57:37 +0530

    These are the API that are need to be created

--- springbootyoutube/src/main/java/com/springbootproject/springbootyoutube/job/JobController.java
+++ springbootyoutube/src/main/java/com/springbootproject/springbootyoutube/job/JobController.java
@@ -6,6 +6,14 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Specidific end points for the jobs:
+ * GET /jobs: Get the jobs
+ * GET /jobs/{id}: Get the specific job bu ID
+ * GET /jobs/{id}/company :Get the specific company associated with the ID
+ * POST /jobs ----> post the values in the array
+ * DELETE /jobs/{ID}: Delete the jobs by the ID
+ */
 
 @RestController
 public class JobController {
