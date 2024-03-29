a9694667a4dc866ee7f5f16d0fd77f6027d24b45
Ajin007 <ajinroch007@gmail.com>
2024-03-29 18:14:14 +0530

    Requestbody added so that the value will be added

--- springbootyoutube/src/main/java/com/springbootproject/springbootyoutube/job/JobController.java
+++ springbootyoutube/src/main/java/com/springbootproject/springbootyoutube/job/JobController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -29,7 +30,8 @@ public class JobController {
     }
 
     @PostMapping("/jobs")
-    public String addJob(Job job){
+    //this response body is needed to pass on the values
+    public String addJob(@RequestBody Job job){
 
         // to understand how this works use postman  and send a json object 
         //shall use jsoninit like to make perfect json format data
